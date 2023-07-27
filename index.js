3ae5b1130acb667e688e2e4cf0e3044cfa690c34
virginiafreitas <virginiacdefreitas@gmail.com>
2023-07-26 17:37:46 -0700

    named the output file

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 const inquirer = require('inquirer'); // Library for interactive prompts
 const fs = require('fs'); // File system module for reading and writing files
 
+//imported the module from shape.js
+const { Triangle, Square, Circle } = require('./lib/shapes.js') // Import custom shape classes corresponding to the object 
+
+const path = require('path'); // Path module for working with file paths
+const filename = "logo.svg"; // Name of the output SVG file
+
