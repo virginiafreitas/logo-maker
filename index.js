cdd8967199bd496bab3892c0b159650978d88627
virginiafreitas <virginiacdefreitas@gmail.com>
2023-07-26 17:42:06 -0700

    array of prompts

--- index.js
+++ index.js
@@ -8,3 +8,37 @@ const { Triangle, Square, Circle } = require('./lib/shapes.js') // Import custom
 const path = require('path'); // Path module for working with file paths
 const filename = "logo.svg"; // Name of the output SVG file
 
+// Array of questions to prompt the user for logo details (shape, text, shape color and text color) as an object
+const questions = [
+  {
+    type: 'input',
+    message: 'Provide a text of up to 3 characters to be in your logo:',
+    name: 'text',
+    validate: function(response) {
+      if (response.length > 3) {
+        console.log("\n-> Please input text with 3 characters maximum.")
+        return;
+      }
+      return true;
+    }
+  },
+  {
+    type: 'input',
+    message: 'Provide the color for the text in your logo:',
+    name: 'fontcolor',
+  },
+  {
+    type: 'list',
+    message: 'Provide the shape you want the logo to be:',
+    name: 'shape',
+    choices: ["circle", "triangle", "square"]
+  },
+  {
+    type: 'input',
+    message: 'Provide the color you want the shape of the logo to be:',
+    name: 'shapecolor',
+  }
+];
+
+
+
